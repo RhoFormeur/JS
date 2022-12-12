575fffcd58170a7571d01d8f09356367edfa89cf
RhoFormeur <ledusmaxime@gmail.com>
2022-12-12 15:55:41 +0100

    add : filter

--- testapi.js
+++ testapi.js
@@ -1,101 +1,88 @@
 const app = document.getElementById(`app`)
 
-// // OMDb Api URL : http://www.omdbapi.com/?apikey=794ce864
-// const requestURL = ``
-
-// const request = new XMLHttpRequest()
-
-// const requestTitre = `&t=` + prompt(`Titre du film`)
-
-// const fullURL = requestURL + requestTitre
-
-// request.open(`GET`, fullURL)
-
-// request.responseType = `json`
-// request.send()
-
-// request.onload = function () {
-//     const film = request.response
-//     console.log(film)
-//     console.log(film[`Title`])
-//     console.log(film[`Poster`])
-//     card(film)
-// }
-// console.log(request)
-
-// function card(jsonObj) {
-//     const imgDiv = document.createElement(`div`)
-//     const infoDiv = document.createElement(`div`)
-//     const img = document.createElement(`img`)
-//     const h3 = document.createElement(`h3`)
-//     const p1 = document.createElement(`p`)
-//     const p2 = document.createElement(`p`)
-//     const p3 = document.createElement(`p`)
-//     const p4 = document.createElement(`p`)
-//     const p5 = document.createElement(`p`)
-//     const p6 = document.createElement(`p`)
-//     const p7 = document.createElement(`p`)
-
-//     img.src = jsonObj[`Poster`]
-//     h3.innerText = `Titre : ` + jsonObj[`Title`]
-//     p1.innerText = `Année : ` + jsonObj[`Year`]
-//     p2.innerText = `Durée : ` + jsonObj[`Runtime`]
-//     p3.innerText = `Genre : ` + jsonObj[`Genre`]
-//     p4.innerText = `Acteurs : ` + jsonObj[`Actors`]
-//     p5.innerText = `Réalisateur : ` + jsonObj[`Director`]
-//     p6.innerText = `Résumé : ` + jsonObj[`Plot`]
-//     p7.innerText = `Note : ` + jsonObj[`Ratings`][0][`Value`] + ` => ` + jsonObj[`Ratings`][0][`Source`]
-
-//     imgDiv.appendChild(img)
-//     infoDiv.appendChild(h3)
-//     infoDiv.appendChild(p1)
-//     infoDiv.appendChild(p2)
-//     infoDiv.appendChild(p3)
-//     infoDiv.appendChild(p4)
-//     infoDiv.appendChild(p5)
-//     infoDiv.appendChild(p6)
-//     infoDiv.appendChild(p7)
-//     app.appendChild(imgDiv)
-//     app.appendChild(infoDiv)
-// }
-
-
-
-
-
 // TMDb API weekly trending URL : https://api.themoviedb.org/3/trending/all/week?api_key=de915367cc4815667c4f4866d61485af&language=fr-FR
 
 const fullURL = `https://api.themoviedb.org/3/trending/all/week?api_key=de915367cc4815667c4f4866d61485af&language=fr-FR`
-app.style.display = `flex`
-app.style.flexWrap = `wrap`
-fetch(fullURL)
-    .then(res => res.json())
-    .then(res => {
-        console.log(`fetch`, res);
-        card(res)
-    })
-    .catch(err => console.log(err))
 
+const cardContainer = document.createElement(`ul`)
+cardContainer.setAttribute(`id`, `cardList`)
+cardContainer.style.display = `flex`
+cardContainer.style.flexWrap = `wrap`
+cardContainer.style.listStyle = `none`
+
+const divSearch = document.createElement(`div`)
+const inputSearch = document.createElement(`input`)
+inputSearch.setAttribute(`type`, `text`)
+inputSearch.setAttribute(`onkeyup`, `filtrer()`)
+divSearch.style.marginBottom = `5px`
+divSearch.appendChild(inputSearch)
+app.appendChild(divSearch)
+app.appendChild(cardContainer)
+
+fetchTable(fullURL)
+
+// Fonction recherche
+function filtrer() {
+    const filter = inputSearch.value.toUpperCase()
+    const ul = document.getElementById(`cardList`)
+    const li = ul.getElementsByTagName(`li`)
+    console.log(li.length);
+    console.log(filter);
+    for (i = 0; i < li.length; i++) {
+        const h3 = li[i].getElementsByTagName(`h3`)[0]
+        const txtvalue = h3.textContent || h3.innerText
+        if (txtvalue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = ``
+        } else {
+            li[i].style.display = `none`
+        }
+    }
+}
+
+// Requete films et series trending
+function fetchTable(url) {
+    fetch(url)
+        .then(res => res.json())
+        .then(res => {
+            console.log(`fetch`, res)
+            card(res[`results`])
+        })
+        .catch(err => console.log(err))
+}
+
+//Fermer la page article
+function closeArticle() {
+    cardContainer.innerHTML = ``
+    fetchTable(fullURL)
+}
+
+// Création des cards en mappant le tableau
 function card(jsonObj) {
-    jsonObj[`results`].map((i) => {
-        const div = document.createElement(`div`)
-        const a = document.createElement(`a`)
+    jsonObj.map((i) => {
+        const div = document.createElement(`li`)
+        const h3 = document.createElement(`h3`)
         const img = document.createElement(`img`)
         const posterPath = i.poster_path
-        // img.setAttribute(`id`, i.id)
-        // img.setAttribute(`type`, i.media_type)
         img.src = `https://image.tmdb.org/t/p/original` + posterPath
-        img.style.width = `200px`
-        img.addEventListener(`click`, () => buildArticle(i.media_type, i.id))
-        div.style.margin = `5px`
-        a.appendChild(img)
-        div.appendChild(a)
-        app.appendChild(div)
+        img.style.width = `300px`
+        img.addEventListener(`click`, () => buildUrl(i.media_type, i.id))
 
+        // Different selon le type d'article pour recuperer le nom
+        if (i.media_type === `tv`) {
+            h3.innerText = i[`name`]
+        } else if (i.media_type === `movie`) {
+            h3.innerText = i[`title`]
+        }
+
+        div.style.margin = `5px`
+        div.appendChild(img)
+        div.appendChild(h3)
+        cardContainer.appendChild(div)
     })
 }
 
-function fetchUrl(url) {
+// Requete details article
+function fetchArticle(url) {
     fetch(url)
         .then(res => res.json())
         .then(res => {
@@ -105,42 +92,32 @@ function fetchUrl(url) {
         .catch(err => console.log(err))
 }
 
-function buildArticle(type, id) {
+// Creatrion de l'URL en fonction de l'article
+function buildUrl(type, id) {
     console.log(id);
     console.log(type);
     let url = ``
     if (type === `tv`) {
         url = `https://api.themoviedb.org/3/tv/` + id + `?api_key=de915367cc4815667c4f4866d61485af&language=fr-FR`
-        fetchUrl(url)
+        fetchArticle(url)
     } else if (type === `movie`) {
         url = `https://api.themoviedb.org/3/movie/` + id + `?api_key=de915367cc4815667c4f4866d61485af&language=fr-FR`
-        fetchUrl(url)
+        fetchArticle(url)
     }
-    // const id = e.target.id
-    // console.log(`event`,e.target)
-    // const type = e.target.type
-    // console.log(`id`, id)
-    // console.log(`type`, type);
-    // switch (url) {
-    //     case type === `tv`:
-    //         url = `https://api.themoviedb.org/3/tv/` + id + `?api_key=de915367cc4815667c4f4866d61485af&language=fr-FR`
-    //         console.log(`prefetch`, url);
-    //         fetchUrl(url)
-    //         break
-    //     case type === `movie`:
-    //         url = `https://api.themoviedb.org/3/movie/` + id + `?api_key=de915367cc4815667c4f4866d61485af&language=fr-FR`
-    //         console.log(`prefetch`, url);
-    //         fetchUrl(url)
-    //         break
-    // }
-
 }
 
-function affichage(jsonObj) {
-    app.innerHTML = ``
+
+// Reset page pour afficher un article
+function affichage(jsonObj,) {
+    cardContainer.innerHTML = ``
     const divImg = document.createElement(`div`)
     const divInfo = document.createElement(`div`)
     const img = document.createElement(`img`)
+    const btn = document.createElement(`button`)
+    btn.setAttribute(`onclick`, `closeArticle()`)
+    btn.innerText = `X`
+    btn.style.width = `50px`
+    btn.style.height = `50px`
     img.src = `https://image.tmdb.org/t/p/original` + jsonObj[`poster_path`]
     img.style.width = `50vw`
     const titre = document.createElement(`p`)
@@ -149,29 +126,37 @@ function affichage(jsonObj) {
     const rate = document.createElement(`p`)
     const nbGenre = jsonObj.genres.length
     const pGenre = document.createElement(`p`)
-    pGenre.innerText=`Genres :`
+    pGenre.innerText = `Genres :`
     pGenre.style.marginRight = `5px`
     divGenre.appendChild(pGenre)
     divGenre.style.display = `flex`
     console.log(nbGenre);
-    titre.innerText = `Titre : ` + jsonObj[`name`]
 
+    // Different selon le type d'article pour recuperer le nom
+    if (jsonObj[`name`] === undefined) {
+        titre.innerText = `Titre : ` + jsonObj[`title`]
+    } else {
+        titre.innerText = `Titre : ` + jsonObj[`name`]
+    }
+
+    // Boucle pour afficher les genres peut importe le nombre
     for (i = 0; i < nbGenre; i++) {
         const p = document.createElement(`p`)
         p.innerText = jsonObj[`genres`][i][`name`]
         p.style.marginRight = `5px`
         divGenre.appendChild(p)
     }
+
     console.log(divGenre.innerText);
-    // genre.innerText = jsonObj[`genres`][0][`name`] + `, ` + jsonObj[`genres`][1][`name`] + `, ` + jsonObj[`genres`][2][`name`]
     resume.innerText = `Résumé : ` + jsonObj[`overview`]
     rate.innerText = `Note : ` + jsonObj[`vote_average`]
 
     divImg.appendChild(img)
+    divImg.appendChild(btn)
     divInfo.appendChild(titre)
     divInfo.appendChild(divGenre)
     divInfo.appendChild(resume)
     divInfo.appendChild(rate)
-    app.appendChild(divImg)
-    app.appendChild(divInfo)
+    cardContainer.appendChild(divImg)
+    cardContainer.appendChild(divInfo)
 }
